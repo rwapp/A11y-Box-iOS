560e08feaed4a1167f8685d2b861bfdcefdf1ad7
rwapp <r.whitaker@mac.com>
2020-02-23 11:38:25 +0000

    UITesting

--- A11y BoxUITests/A11y_BoxUITests.swift
+++ A11y BoxUITests/A11y_BoxUITests.swift
@@ -11,32 +11,40 @@ import XCTest
 //swiftlint:disable:next type_name
 class Developing_Inclusive_Mobile_AppsUITests: XCTestCase {
 
+    let app = XCUIApplication()
+
     override func setUp() {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-
-        // In UI tests it is usually best to stop immediately when a failure occurs.
-        continueAfterFailure = false
-    }
-
-    override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
-    func testExample() {
-        // UI tests must launch the application that they test.
-        let app = XCUIApplication()
         app.launch()
-
-        // Use recording to get started writing UI tests.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
     }
 
-    func testLaunchPerformance() {
-        if #available(macOS 10.15, iOS 13.0, tvOS 13.0, *) {
-            // This measures how long it takes to launch your application.
-            measure(metrics: [XCTOSSignpostMetric.applicationLaunch]) {
-                XCUIApplication().launch()
-            }
+    func test_buttonAllButtons() {
+        // Navigate to the screen you want to check
+        XCUIApplication().tabBars.buttons["Tests"].tap()
+
+        let buttons = app.buttons.allElementsBoundByIndex
+
+        for button in buttons {
+            check(button: button)
+        }
+    }
+
+    // Listing 11-1
+    func check(button: XCUIElement) {
+        XCTAssert(button.frame.size.height >= 44)
+        XCTAssert(button.frame.size.width >= 44)
+        XCTAssertTrue(button.label.count <= 40)
+        XCTAssert(button.label.first!.isUppercase)
+    }
+
+    // Listing 11-2
+    func test_imageName() {
+        // Navigate to the screen you want to check
+        XCUIApplication().tabBars.buttons["Tests"].tap()
+
+        let images = app.images.allElementsBoundByIndex
+
+        for image in images {
+            XCTAssertFalse(image.label.contains("image"))
         }
     }
 }
