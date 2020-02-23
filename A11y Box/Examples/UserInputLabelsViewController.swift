b2dac68d14d70fee441e6aee6dee186f813e30ca
rwapp <r.whitaker@mac.com>
2020-02-23 10:50:33 +0000

    voice control input labels

---
+++ A11y Box/Examples/UserInputLabelsViewController.swift
@@ -0,0 +1,50 @@
+//
+//  UserInputLabelsViewController.swift
+//  A11y Box
+//
+//  Created by Rob Whitaker on 23/02/2020.
+//  Copyright © 2020 RWAPP. All rights reserved.
+//
+
+import UIKit
+
+class UserInputLabelsViewController: UIViewController {
+
+    @IBOutlet weak private var playButton: UIButton!
+    lazy var alert = alertView()
+
+    private let songTitle = "Shake It Off"
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Listing 9-5
+        // Note: These strings should be localized
+        playButton?.accessibilityLabel = "Play \(songTitle)"
+        playButton?.accessibilityUserInputLabels = ["Play",
+                                                    "Play song",
+                                                    "Play track",
+                                                    "Play \(songTitle)"]
+    }
+
+    @IBAction
+    func playPressed() {
+        view.addSubview(alert)
+    }
+
+    private func alertView() -> CustomModalAlert {
+        let alert = CustomModalAlert(frame: view.frame)
+        alert.delegate = self
+        alert.accessibilityViewIsModal = true
+
+        alert.center = view.center
+        self.alert = alert
+        return alert
+    }
+}
+
+extension UserInputLabelsViewController: CustomModalAlertDelegate {
+    func dismiss() {
+        alert.removeFromSuperview()
+    }
+}
