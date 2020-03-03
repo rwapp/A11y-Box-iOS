b28996a9f3eea6124746354ef553623a99730874
Martin Wright <martin@tinyshinyapps.co.uk>
2020-03-03 22:17:26 +0000

    Added component traversal example

--- A11y Box/Examples/TraversalOrderViewController.swift
+++ A11y Box/Examples/TraversalOrderViewController.swift
@@ -12,5 +12,11 @@ class TraversalOrderViewController: UIViewController {
 
     @IBOutlet weak private var orderedTraversal: OrderedTraversalView!
     @IBOutlet weak private var unorderedTraversal: UnorderedTraversalView!
-
+    @IBOutlet weak private var componentTraversal: ComponentTraversalView!
+    
+    override func viewDidLoad() {
+        self.componentTraversal.tileDetails = [("Heading 1", "Detail 1"),
+                                               ("Heading 2 ", "Detail 2"),
+                                               ("Heading 3", "Detail 3")]
+    }
 }
---
+++ A11y Box/Views/ComponentTraversalView.swift
@@ -0,0 +1,40 @@
+//
+//  ComponentTraversalView.swift
+//  A11y Box
+//
+//  Created by Martin Wright on 29/02/2020.
+//  Copyright © 2020 RWAPP. All rights reserved.
+//
+
+import UIKit
+
+@IBDesignable
+class ComponentTraversalView: UIView {
+
+    @IBOutlet weak var tile1: TileView!
+    @IBOutlet weak var tile2: TileView!
+    @IBOutlet weak var tile3: TileView!
+    var tileDetails: [(String, String)]? {
+        didSet {
+            updateView()
+        }
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        let view = UINib(nibName: "ComponentTraversalView",
+                         bundle: Bundle.main).instantiate(withOwner: self,
+                                                          //swiftlint:disable:next force_cast
+                            options: nil).first as! UIView
+        view.frame = bounds
+        addSubview(view)
+    }
+    
+    func updateView() {
+        if let tiles = tileDetails {
+            tile1.tileDetails = tiles[0]
+            tile2.tileDetails = tiles[1]
+            tile3.tileDetails = tiles[2]
+        }
+    }
+}
